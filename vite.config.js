4d6e00ba9ccd71c1b173457dc59797c885bb0d1e
Daiana Poza <daianapoza@gmail.com>
2026-08-26 18:10:42 -0300

    cambio de vinculo

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const repo = "mai.tipaldo";
+const repo = "psicologa-maira-tipaldo";
 
 export default defineConfig({
   plugins: [react()],
